b3e694e111dc091695f0a11ef6c9d0774724d5fc
rajesh-fs <Rajesh.Devarasetti@firstgroup.com>
2025-09-12 15:49:55 +0530

    Clean up (#1)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import DetailsPage from './pages/DetailsPage';
-import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <Router>
-      <div style={{
-        maxWidth: '800px',
-        margin: '0 auto',
-        padding: '2rem',
-        textAlign: 'center'
-      }}>
-        <h1>React Series Tracker</h1>
-
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/details/:id" element={<DetailsPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <h1>Welcome to Rateit</h1>
   );
 }
 
